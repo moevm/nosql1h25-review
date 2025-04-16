faa1f2e359ffcec43fec2398be273881e9d6d4e9
Алина <alina.valeyeva21@yandex.ru>
2025-04-16 19:14:10 +0300

    fixed templates and added a template for the homepage

--- src/backend/rotten_scores/user_profile/urls.py
+++ src/backend/rotten_scores/user_profile/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.my_rating_and_reviews, name='my_reviews_and reviews'),
+    path('', views.my_reviews_and_reviews, name='my_reviews_and_reviews'),
     path('account/', views.account, name='account'),
-    path('statistics/', views.statistics_view, name='statistics'),
+    path('statistics/', views.statistics, name='statistics'),
 ]
--- src/backend/rotten_scores/user_profile/views.py
+++ src/backend/rotten_scores/user_profile/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def my_reviews_and(request):
+def my_reviews_and_reviews(request):
     return HttpResponse("Заглушка")
 def account(request):
     return HttpResponse("Заглушка")
