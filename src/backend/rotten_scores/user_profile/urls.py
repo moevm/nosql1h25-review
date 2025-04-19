25113f0170d28ebeb58d9a00d8477362822eef51
Egor Kuzmihykh <egor67877@gmail.com>
2025-04-19 13:31:03 +0300

    added base_profile template and some html files

--- src/backend/rotten_scores/user_profile/urls.py
+++ src/backend/rotten_scores/user_profile/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('', views.my_reviews_and_reviews, name='my_rating_and_reviews'),
     path('account/', views.account, name='account'),
     path('statistics/', views.statistics, name='statistics'),
+    path('admin_panel/',views.admin_panel, name='admin_panel'),
+    path('load_section/', views.load_section, name='load_section'),
 ]
