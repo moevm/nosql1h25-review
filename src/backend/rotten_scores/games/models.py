6ca4b4fcac72b3f4abf32bd342a67404df2a3b81
Борисов Иван Павлович <borisov.bip@gmail.com>
2025-04-20 18:27:36 +0300

    search

--- src/backend/rotten_scores/games/models.py
+++ src/backend/rotten_scores/games/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Game(models.Model):
+    title = models.CharField(max_length=255, db_index=True)  # индекс по названию
+    cover_url = models.URLField(null=True, blank=True)       # ссылка на обложку
+    release_date = models.DateField(null=True, blank=True)   # дата выхода
+    critic_score = models.FloatField(null=True, blank=True)  # средняя оценка критиков
+
+    def __str__(self):
+        return self.title
--- src/backend/rotten_scores/games/views.py
+++ src/backend/rotten_scores/games/views.py
@@ -1,43 +1,51 @@
-from django.views.generic import TemplateView
+from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import JsonResponse
+from .models import Game
+import logging
 from pymongo import MongoClient
 from django.conf import settings
-from datetime import datetime
-import sys
-import os
-from django.shortcuts import render, get_object_or_404
-from bson.objectid import ObjectId
 
-client = MongoClient(settings.MONGODB_URI)
-db = client[settings.MONGODB_NAME]
-
-# при открытии Games почему-то хэдер, футер и список троиться, поэтому пока заглушка
 def game_list(request):
-    return HttpResponse("Заглушка лист игр")
-# def game_list(request):
-#     games = list(db.games.find().limit(20))  # Можно добавить фильтры и пагинацию
-    
-#     for game in games:
-#         game['id'] = str(game['_id'])
+    # return HttpResponse("Заглушка лист игр")
+    query = request.GET.get('q', '')
+    if query:
+        games = Game.objects.filter(title__icontains=query)
+    else:
+        games = Game.objects.all()
+    return render(request, 'game_list.html', {'games': games, 'query': query})
 
-#         if 'releaseDate' in game and isinstance(game['releaseDate'], datetime):
-#             game['release_date_formatted'] = game['releaseDate'].strftime('%Y-%m-%d')
+def game_detail(request):
+    return HttpResponse("Заглушка детали игры")
 
-#     return render(request, 'games/game_list.html', {'games': games})
+logger = logging.getLogger(__name__)
 
-
-def game_detail(request, pk):
+def search_games(request):
     try:
-        game = db.games.find_one({'_id': ObjectId(pk)})
-    except Exception:
-        game = None
+        client = MongoClient(settings.MONGODB_URI)
+        db = client[settings.MONGODB_NAME]
 
-    if not game:
-        return render(request, '404.html', status=404)
+        query = request.GET.get('q', '').strip()
+        logger.info(f"Search query: {query}")
 
-    game['id'] = str(game['_id'])
+        if 'games' not in db.list_collection_names():
+            logger.error("Collection 'games' not found!")
+            return JsonResponse([], safe=False)
+        
+        results = db.games.find(
+            {"title": {"$regex": query, "$options": "i"}},
+            {"title": 1, "cover_url": 1, "release_date": 1, "critic_score": 1}
+        ).limit(5)
 
-    if 'releaseDate' in game and isinstance(game['releaseDate'], datetime):
-        game['release_date_formatted'] = game['releaseDate'].strftime('%Y-%m-%d')
+        games = []
+        for game in results:
+            game['id'] = str(game.pop('_id'))
+            if 'release_date' in game:
+                game['release_date'] = game['release_date'].strftime('%Y-%m-%d')
+            games.append(game)
 
-    return render(request, 'games/game_detail.html', {'game': game})
+        return JsonResponse(games, safe=False)
+
+    except Exception as e:
+        print(f"Ошибка: {e}")
+        return JsonResponse([], safe=False)
