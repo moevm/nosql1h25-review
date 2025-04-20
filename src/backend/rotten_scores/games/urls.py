b78f1c15c1fbe160c5fd61113216c6d32e777353
Борисов Иван Павлович <borisov.bip@gmail.com>
2025-04-20 18:27:36 +0300

    search

--- src/backend/rotten_scores/games/urls.py
+++ src/backend/rotten_scores/games/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.game_list, name='game_list'),
-    path('<str:pk>/', views.game_detail, name='game_detail'),
+    path('<int:pk>/', views.game_detail, name='game_detail'),
+    path('search/', views.search_games, name='search_games'),
 ]
--- src/backend/rotten_scores/rotten_scores/urls.py
+++ src/backend/rotten_scores/rotten_scores/urls.py
@@ -9,8 +9,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(('core.urls', 'core'), namespace='core')),
     path('profile/', include(('user_profile.urls', 'user_profile'), namespace='user_profile')),
-    path('profile/admin_panel/', include(('admin_panel.urls', 'admin_panel'), namespace='admin_panel')),
     path('games/', include(('games.urls', 'games'), namespace='games')),
+    path('games/', include('games.urls')),
     path('reviews/', include(('reviews.urls', 'reviews'), namespace='reviews')),
 ]
 
