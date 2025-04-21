ca973ad86fa1b70ae9110a4863c51a4dee5f7ced
Борисов Иван Павлович <borisov.bip@gmail.com>
2025-04-22 01:27:44 +0300

    cards with review

--- src/backend/rotten_scores/user_profile/views.py
+++ src/backend/rotten_scores/user_profile/views.py
@@ -1,13 +1,49 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseNotFound
-from django.views.generic import TemplateView
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib import messages
-from .forms import ChangePersonalDataForm, ChangePasswordForm
+from pymongo import MongoClient
+from django.conf import settings
+from .models import GameReview
+from datetime import datetime
 
 
-def my_rating_and_reviews(request):
-    return render(request, 'profile/base_profile.html')
+def my_ratings_and_reviews(request):
+    client = MongoClient(settings.MONGODB_URI)
+    db = client[settings.MONGODB_NAME]
+    user_id = request.user.id
+
+    data = [
+  {
+    "$match": {"userId": user_id }
+  },
+  {
+    "$lookup": {
+      "from": "games",
+      "localField": "gameId",
+      "foreignField": "_id",
+      "as": "game_info"
+    }
+  },
+  {
+    "$project": {
+      "_id": 0,
+      "text": 1,
+      "rating": 1,
+      "createdAt": 1,
+      "game_info.title": 1,
+      "game_info.imageUrl": 1
+    }
+  }
+]
+
+    reviews = list(db.user_reviews.aggregate(data))
+
+    print(reviews)
+
+    context = {
+        'reviews': reviews,
+    }
+
+    return render(request, 'profile/base_profile.html', context)
 
 
 def custom_logout(request):
@@ -15,6 +51,7 @@ def custom_logout(request):
         # Получаем текущего пользователя и вызываем его метод logout
         user = request.user
         user.logout()
+
 
         # Очищаем сессию
         request.session.flush()
@@ -38,7 +75,6 @@ def load_section(request):
         return render(request, template_name)
     except:
         return HttpResponseNotFound("Section not found")
-
 
 def account(request):
     personal_form = ChangePersonalDataForm(data=request.POST, user=request.user)
