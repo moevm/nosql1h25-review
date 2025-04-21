6e7ae8254cd53afe959e35f9b5ce11e30328738f
Борисов Иван Павлович <borisov.bip@gmail.com>
2025-04-22 01:27:44 +0300

    cards with review

--- src/backend/rotten_scores/user_profile/views.py
+++ src/backend/rotten_scores/user_profile/views.py
@@ -1,8 +1,51 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseNotFound
+from pymongo import MongoClient
+from django.conf import settings
+from .models import GameReview
+from datetime import datetime
 
-def my_reviews_and_reviews(request):
-    return render(request, 'profile/base_profile.html')
+
+def my_ratings_and_reviews(request):
+    client = MongoClient(settings.MONGODB_URI)
+    db = client[settings.MONGODB_NAME]
+    user_id = request.user.id
+
+    data = [
+  {
+    "$match": {"userId": user_id }
+  },
+  {
+    "$lookup": {
+      "from": "games",
+      "localField": "gameId",
+      "foreignField": "_id",
+      "as": "game_info"
+    }
+  },
+  {
+    "$project": {
+      "_id": 0,
+      "text": 1,
+      "rating": 1,
+      "createdAt": 1,
+      "game_info.title": 1,
+      "game_info.imageUrl": 1
+    }
+  }
+]
+
+    reviews = list(db.user_reviews.aggregate(data))
+
+    print(reviews)
+
+    context = {
+        'reviews': reviews,
+    }
+
+    return render(request, 'profile/base_profile.html', context)
+
+
 
 
 def custom_logout(request):
@@ -16,9 +59,10 @@ def custom_logout(request):
         request.session.flush()
 
     return redirect('core:homepage')
+
+
 def load_section(request):
     section = request.GET.get('section', 'profile')
-
 
     section_mapping = {
         'ratings': 'ratings.html',
@@ -33,6 +77,7 @@ def load_section(request):
         return render(request, template_name)
     except:
         return HttpResponseNotFound("Section not found")
+
 def account(request):
     return HttpResponse("Заглушка")
 def statistics(request):
