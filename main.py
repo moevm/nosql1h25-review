3ff97852dd07bfbbfbff7db8215bb46e7ae421b0
neepaw <bangerwockeez@gmail.com>
2025-05-19 21:20:36 +0300

    Add useful import/export functions to main.py

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
-from src.io import MongoImporter
+from src.io import MongoImporter, MongoExporter
+
+
+def export_data():
+    mongo_io_manager = MongoExporter("game_reviews_db", 'backup')
+    mongo_io_manager.export_data()
+
+
+def import_data():
+    mongo_io_manager = MongoImporter("game_reviews_db")
+    mongo_io_manager.import_data('backup')
 
 
 def main():
-    mongo_io_manager = MongoImporter("game_reviews_db")
-    mongo_io_manager.import_data("backup/game_reviews_db", drop=True)
+    export_data()
 
 
 if __name__ == '__main__':
